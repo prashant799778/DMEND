ff68a153d5f1c6fa0d779595378a6903ffaa8a4a
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-25 15:57:39 +0530

    test

---
+++ adeb.py
@@ -0,0 +1,528 @@
+from flask import Flask,request,abort
+from flask import Flask, send_from_directory, abort
+import uuid
+import json
+import json
+import math, random
+import numpy as np
+import pymysql
+import requests
+import json
+import pymysql
+from flask_cors import CORS
+from datetime import datetime
+from config import Connection
+import databasefile
+from config import Connection
+import commonfile
+import ConstantData
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import re
+import razorpay
+
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+
+
+
+@app.route('/userSignup', methods=['POST'])
+def userSignup():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
+        startlimit,endlimit="",""
+        keyarr = ['mobileNo','deviceKey']
+        commonfile.writeLog("userSignup",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+       
+        if msg == "1":
+            
+            column,values="",""
+            
+           
+            
+            mobileNo=inputdata["mobileNo"]
+            deviceKey=inputdata["deviceKey"]
+            usertypeId="2"
+            
+          
+            
+            digits = "0123456789"
+            otp = " "
+            for i in range(4):
+                otp += digits[math.floor(random.random() * 10)]
+
+           
+
+            UserId = (commonfile.CreateHashKey(mobileNo,userTypeId)).hex
+            
+            
+            WhereCondition = " and mobileNo = '" + str(mobileNo) + "'"
+            count = databasefile.SelectCountQuery("userMaster",WhereCondition,"")
+            
+            if int(count) > 0:
+                WhereCondition = " mobileNo = '" + str(mobileNo) + "'"
+                column = " otp = '" + str(otp)  + "'"
+                updateOtp = databasefile.UpdateQuery("userMaster",column,WhereCondition)
+                print(updateOtp,'updatedata')
+                if updateOtp != "0":
+                    column = '*'
+                    data = databasefile.SelectQuery("userMaster",column,WhereCondition)                  
+                    print(data,"===================")
+                    return data
+                else:
+                    return commonfile.Errormessage()
+                
+            else:
+               
+
+                if 'email' in inputdata:
+                    email=inputdata["email"]
+                    column=column+" ,email"
+                    values=values+"','"+str(email)
+                if 'password' in inputdata:
+                    password=inputdata["password"]
+                    column=column+" ,password"
+                    values=values+"','"+str(password)
+                if 'name' in inputdata:
+                    name=inputdata["name"]
+                    column=column+" ,name"
+                    values=values+"','"+str(name)
+
+
+
+ 
+                currentLocationlatlong=""
+
+                column="mobileNo,userId,otp,userTypeId,deviceKey"+column
+                
+                
+                values=  "'"+str(mobileNo)+"','"+str(UserId)+"','"+str(otp)+"','"+str('2')+"','"+str(deviceKey)+values+ "'"
+                data=databasefile.InsertQuery("userMaster",column,values)
+             
+
+                if data != "0":
+                    column = '*'
+                    
+                    data = databasefile.SelectQuery2("userMaster",column,WhereCondition,"",startlimit,endlimit)
+                    print(data)
+                    Data = {"status":"true","message":"","result":data["result"][0]}                  
+                    return Data
+                else:
+                    return commonfile.Errormessage()
+                        
+        else:
+            return msg 
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"status":"false","message":"something went wrong","result":""}
+        return output
+
+
+@app.route('/userVerifyOtp', methods=['POST'])
+def userverifyOtp():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['otp','mobileNo']
+        print(inputdata,"B")
+        commonfile.writeLog("userverifyOtp",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg =="1":
+            otp=str(inputdata['otp'])
+            mobileNo=str(inputdata['mobileNo'])
+
+            column="mobileNo,otp,userId,userTypeId"
+            whereCondition= "  otp='" + otp+ "' and mobileNo='" + mobileNo+"'"
+            verifyOtp=databasefile.SelectQuery(" userMaster ",column,whereCondition)
+            print("verifyOtp======",verifyOtp)
+            if  (verifyOtp["status"]!="false") or verifyOtp!=None: 
+                return verifyOtp
+            else:
+                return verifyOtp 
+        else:
+            return msg         
+ 
+    except KeyError :
+        print("Key Exception---->")   
+        output = {"result":"key error","status":"false"}
+        return output  
+
+    except Exception as e :
+        print("Exceptio`121QWAaUJIHUJG n---->" +str(e))    
+        output = {"result":"somthing went wrong","status":"false"}
+        return output
+
+
+@app.route('/updateuserProfile', methods=['POST'])
+def updateDriverProfile():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data()) 
+        startlimit,endlimit="",""
+        keyarr = ["name","email","password",'userId']
+        commonfile.writeLog("updateuserProfile",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+       
+        if msg == "1":
+            name,email,password,userTypeId,mobileNo,gender="","","","","",""
+            column,values="",""
+            
+          
+
+            
+            if 'email' in inputdata:
+                email=inputdata["email"]
+                column=" email='"+str(email)+"' " 
+            if 'name' in inputdata:
+                name=inputdata["name"]
+                column=column+" ,name='"+str(name)+"' "  
+            if 'password' in inputdata:
+                password=inputdata["password"]
+                column=column+" ,password= '"+str(password)+"' "                
+            if 'mobileNo' in inputdata:
+                mobileNo=inputdata["mobileNo"]
+                column=column+" ,mobileNo='"+str(mobileNo)+"' "
+
+            if 'gender' in inputdata:
+                gender=inputdata["gender"]
+                column=column+" ,gender='"+str(gender)+"' "   
+
+            if 'userId' in inputdata:
+                userId=inputdata["userId"]    
+                
+            
+            whereCondition= " userId= '"+str(userId)+"' and userTypeId='2' "
+          
+            data=databasefile.UpdateQuery("userMaster",column,whereCondition)
+         
+
+            if data != "0":
+                Data = {"status":"true","message":"data Updated Successfully","result":"data Updated Successfully"}                  
+                return Data
+            else:
+                return commonfile.Errormessage()
+                        
+        else:
+            return msg 
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"status":"false","message":"something went wrong","result":""}
+        return output        
+
+
+
+
+
+#user Login
+@app.route('/userLogin', methods=['POST'])
+def userlogin():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['password','mobileNo']
+        commonfile.writeLog("Login",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg == "1":
+            mobileNo = inputdata["mobileNo"]
+            password = inputdata["password"]
+            column=  "us.mobileNo,us.name,us.userId,um.name as userName"
+            whereCondition= "us.mobileNo = '" + str(mobileNo) + "' and us.password = '" + password + "' and us.userTypeId=um.id"
+            loginuser=databasefile.SelectQuery1("userMaster as us,usertypeMaster as um",column,whereCondition)
+            if (loginuser!=0):   
+                Data = {"result":loginuser,"status":"true"}                  
+                return Data
+            else:
+                data={"status":"Failed","result":"Login Failed"}
+                return data
+
+        else:
+            return msg 
+    except KeyError as e:
+        print("Exception---->" +str(e))        
+        output = {"result":"Input Keys are not Found","status":"false"}
+        return output    
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"result":"something went wrong","status":"false"}
+        return output
+
+#user can see his wallet Balance         
+
+@app.route('/userWallet', methods=['POST'])
+def userWallet():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['mobileNo','userId']
+        commonfile.writeLog("userWallet",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg == "1":
+            mobileNo = inputdata["mobileNo"]
+            userId=inputdata['userId']
+           
+            column=  "us.walletBalance  as money"
+            whereCondition= "us.mobileNo = '" + str(mobileNo) + "'and us.userTypeId=um.id and us.userId='" + str(mobileNo) + "'"
+            loginuser=databasefile.SelectQuery1("userMaster as us,usertypeMaster as um",column,whereCondition)
+            if (loginuser!=0):
+                Data = {"result":loginuser,"status":"true"}                  
+                return Data
+            else:
+                data={"status":"Failed","result":"Login Failed"}
+                return data
+
+        else:
+            return msg 
+    except KeyError as e:
+        print("Exception---->" +str(e))        
+        output = {"result":"Input Keys are not Found","status":"false"}
+        return output    
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"result":"something went wrong","status":"false"}
+        return output
+
+#user Can add Money to his wallet
+
+@app.route('/addMoney', methods=['POST'])
+def addmoney():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['mobileNo','userId']
+        commonfile.writeLog("userWallet",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg == "1":
+            mobileNo = inputdata["mobileNo"]
+            userId=inputdata['userId']
+            money=inputdata['money']
+           
+            column=  "us.walletBalance  as money"
+            whereCondition= "us.mobileNo = '" + str(mobileNo) + "'and us.userTypeId=um.id and us.userId='" + str(mobileNo) + "'"
+            loginuser=databasefile.SelectQuery1("userMaster as us,usertypeMaster as um",column,whereCondition)
+            if (loginuser!=0):
+                money1=loginuser1['money']
+                totalMoney=money1+money
+                columns="walletBalance='"+str(totalMoney)+"'"
+                whereCondition=  " mobileNo = '" + str(mobileNo) + "' and userId='" + str(mobileNo) + "' "
+                addmoney=databasefile.UpdateQuery('userMaster',columns,whereCondition)
+
+
+                Data = {"result":loginuser,"status":"true"}                  
+                return Data
+            else:
+                data={"status":"Failed","result":"Login Failed"}
+                return data
+
+        else:
+            return msg 
+    except KeyError as e:
+        print("Exception---->" +str(e))        
+        output = {"result":"Input Keys are not Found","status":"false"}
+        return output    
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"result":"something went wrong","status":"false"}
+        return output
+
+
+
+@app.route('/generateOtp', methods=['POST'])
+def generateOtp():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['email']
+        print(inputdata,"B")
+        commonfile.writeLog("generateOtp",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg =="1":
+            email=str(inputdata["email"])
+
+            digits = "0123456789"
+            OTP = ""
+            for i in range(4):
+                OTP += digits[math.floor(random.random() * 10)]
+            message = Mail(
+                from_email = 'medparliament@medachievers.com',
+                to_emails = str(email),
+                subject = "Otp for Reset Password",
+                html_content = '<strong> Otp To Reset Your Password is:' + str(OTP) + ' </strong> <br> <br> Thanks<br> <br> MedParliament Team')
+            sg = SendGridAPIClient('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')
+            response = sg.send(message)
+           
+
+
+          
+            column="otp='" + str(OTP)+ "'"
+            whereCondition= "  and email = '" + str(email)+ "' "
+            output=databasefile.UpdateQuery("userMaster",column,whereCondition)
+            columns='otp'
+            
+            data=databasefile.SelectQuery("userMaster",columns,whereCondition,"",startlimit,endlimit)
+            if data['result']!="":
+                Data = {"status":"true","message":"","result":data["result"]}                  
+                return Data
+            else:
+                return {"status":"false","message":"Invalid Email","result":""}  
+        else:
+            return msg         
+ 
+    except KeyError :
+        print("Key Exception---->")   
+        output = {"result":"key error","status":"false"}
+        return output  
+
+    except Exception as e :
+        print("Exceptio`121QWAaUJIHUJG n---->" +str(e))    
+        output = {"result":"somthing went wrong","status":"false"}
+        return output
+
+@app.route('/updatePassword', methods=['POST'])
+def updatePassword():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['email','password']
+        print(inputdata,"B")
+        commonfile.writeLog("updatePassword",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg =="1":
+            email=str(inputdata["email"])
+            password=str(inputdata["password"])
+         
+            column="password='" + password+ "'"
+            whereCondition= "  and email = '" + str(email)+ "' "
+            output=databasefile.UpdateQuery("userMaster",column,whereCondition)
+                       
+            if output!='0':
+                Data = {"status":"true","message":commonfile.Successmessage('update'),"result":""}                   
+                return Data
+            else:
+                return commonfile.Errormessage()    
+        else:
+            return msg         
+ 
+    except KeyError :
+        print("Key Exception---->")   
+        output = {"result":"key error","status":"false"}
+        return output  
+
+    except Exception as e :
+        print("Exceptio`121QWAaUJIHUJG n---->" +str(e))    
+        output = {"result":"somthing went wrong","status":"false"}
+        return output
+
+
+
+@app.route('/verifyOtp1', methods=['POST'])
+def verifyOtp1():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['otp','email']
+        print(inputdata,"B")
+        commonfile.writeLog("verifyOtp1",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg =="1":
+            otp=str(inputdata['otp'])
+            email=str(inputdata['email'])
+
+            column="email"
+            whereCondition= " and otp='" + otp+ "' and email='" + email+ "'  "
+            data1=databasefile.SelectQuery("userMaster",column,whereCondition,"",startlimit,endlimit)
+            if  (data1["status"]!="false"):   
+                Data = {"status":"true","message":"","result":data1["result"]}                  
+                return Data
+            else:
+                data = {"status":"false","message":"Invalid OTP","result":""}
+                return data      
+        else:
+            return msg         
+ 
+    except KeyError :
+        print("Key Exception---->")   
+        output = {"result":"key error","status":"false"}
+        return output  
+
+    except Exception as e :
+        print("Exceptio`121QWAaUJIHUJG n---->" +str(e))    
+        output = {"result":"somthing went wrong","status":"false"}
+        return output         
+#_________________-admin____________________-
+#admin _______Login
+
+
+@app.route('/adminLogin', methods=['POST'])
+def adminLogin():
+    try:
+        inputdata =  commonfile.DecodeInputdata(request.get_data())
+        startlimit,endlimit="",""
+        keyarr = ['email','password']
+        commonfile.writeLog("adminLogin",inputdata,0)
+        msg = commonfile.CheckKeyNameBlankValue(keyarr,inputdata)
+        if msg == "1":
+            email = inputdata["email"]
+            password = inputdata["password"]
+            column=  "us.mobileNo,us.name,us.userTypeId,um.usertype,us.userId"
+            whereCondition= " us.email = '" + str(email) + "' and us.password = '" + str(password) + "'  and  us.userTypeId=um.id"
+            loginuser=databasefile.SelectQuery("userMaster as us,usertypeMaster as um",column,whereCondition)
+            if (loginuser['status']!='false'):   
+                               
+                return loginuser
+            else:
+            	loginuser = {"status":"False","message":"Please enter correct password & email","result":""}
+                
+                return loginuser
+        else:
+            return msg 
+    except KeyError as e:
+        print("Exception---->" +str(e))        
+        output = {"result":"Input Keys are not Found","status":"false"}
+        return output    
+    except Exception as e :
+        print("Exception---->" +str(e))           
+        output = {"result":"something went wrong","status":"false"}
+        return output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    CORS(app, support_credentials=True)
+    app.run(host='0.0.0.0',port=5034,debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
