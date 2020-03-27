0696d2a7a2a7255f951f264fdae3f4f5195da691
prashant <prashantgoyal.fourbrick@gmail.com>
2020-03-27 12:30:21 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,7 +5,7 @@ def DBconnection():
     mysqlcon = pymysql.connect(host='localhost',
                             user='root',
                             password='OHAvgawzUw##32',
-                            db='medParliament',
+                            db='adeb',
                             charset='utf8mb4',
                             cursorclass=pymysql.cursors.DictCursor)
     
